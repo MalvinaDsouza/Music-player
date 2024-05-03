6903d77f6a06aec91d222636f2fd7267b8605856
MalvinaDsouza <malvina.dsouza21@gmail.com>
2024-05-03 09:32:45 +0200

    correct

--- server.js
+++ server.js
@@ -1,54 +1,41 @@
 const express = require('express');
-const mysql = require('promise-mysql');
+const mysql = require('mysql2');
 
 const app = express();
 const PORT = process.env.PORT || 6000;
 
-const createTcpPool = async config => {
-  return mysql.createPool({
-    user: 'root', // Replace with your MySQL username
-    password: '12345', // Replace with your MySQL password
-    database: 'music', // Replace with your database name
-    host: '35.242.201.102', // Replace with your Cloud SQL public IP address
-    // Specify additional properties here.
-    ...config,
-  });
-};
+const connection = mysql.createConnection({
+  host: '34.107.31.234',
+  user: 'root',
+  password: '12345',
+  database: 'music'
+});
 
-const initializePool = async () => {
-  try {
-    const pool = await createTcpPool();
-    return pool;
-  } catch (error) {
-    console.error('Error initializing connection pool:', error);
-    throw error;
+// Connect to the MySQL database
+connection.connect((error) => {
+  if (error) {
+    console.error('Error connecting to MySQL database:', error);
+  } else {
+    console.log('Connected to MySQL database!');
+    
+    // Query to retrieve tables
+    connection.query('SHOW TABLES', (err, rows) => {
+      if (err) {
+        console.error('Error retrieving tables:', err);
+      } else {
+        console.log('Tables in the database:');
+        rows.forEach(row => {
+          console.log(row[`Tables_in_${connection.config.database}`]);
+        });
+      }
+      
+      // Close the connection after querying tables
+      connection.end();
+    });
   }
-};
+});
 
-// Initialize the connection pool
-initializePool().then(pool => {
-  // API Endpoint for searching music
-  app.get('/api/music/search', async (req, res) => {
-    const searchTerm = req.query.q; // Get the search term from query parameters
-
-    try {
-      const connection = await pool.getConnection();
-      const [rows] = await connection.query(
-        `SELECT * FROM playlist WHERE artist_name LIKE '%${searchTerm}%' OR track_name LIKE '%${searchTerm}%'`
-      );
-      connection.release();
-      res.json(rows);
-    } catch (error) {
-      console.error('Error searching music:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-
-  // Start server
-  app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-  });
-}).catch(error => {
-  console.error('Error initializing connection pool:', error);
-  process.exit(1);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
